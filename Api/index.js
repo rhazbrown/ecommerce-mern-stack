1135dc45514d843d6398e65b4898cb5be47332e5
rhazbrown <rhazbrown@gmail.com>
2023-08-28 20:34:24 +0100

    cors update

--- Api/index.js
+++ Api/index.js
@@ -18,7 +18,13 @@ mongoose
     console.log(err);
   });
 
-app.use(cors());
+app.use(
+  cors({
+    origin: ["https://ecommerce-mern-stack-api.vercel.app"],
+    methods: ["POST", "GET"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
