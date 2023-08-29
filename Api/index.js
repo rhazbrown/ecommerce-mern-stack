153f55c60a9451195c2de4a1adaeb2fa2669a468
rhazbrown <rhazbrown@gmail.com>
2023-08-29 22:30:09 +0100

    required

--- Api/index.js
+++ Api/index.js
@@ -19,7 +19,7 @@ mongoose
   });
 
 app.use(cors());
-app.use("/", "helooo");
+app.use("/", "heloooo");
 app.use("/api/products", productRoute);
 app.use(express.json());
 app.use("/api/auth", authRoute);
