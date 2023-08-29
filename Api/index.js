ad0d87758b56507f7398670bceffd054fea5a528
rhazbrown <rhazbrown@gmail.com>
2023-08-29 22:13:38 +0100

    update

--- Api/index.js
+++ Api/index.js
@@ -26,7 +26,7 @@ const corsConfig = {
 };
 app.use(cors(corsConfig));
 app.options("", cors(corsConfig));
-app.use("/", "helo");
+app.use("/", "helo0");
 app.use("/api/products", productRoute);
 app.use(express.json());
 app.use("/api/auth", authRoute);
