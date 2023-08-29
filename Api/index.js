35cc8e1a3e3ba83325f4d6efdfac3e9a940772d7
rhazbrown <rhazbrown@gmail.com>
2023-08-29 20:16:51 +0100

    test

--- Api/index.js
+++ Api/index.js
@@ -31,7 +31,7 @@ app.get("/", (req, res) => {
 app.use(express.json());
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
-app.get("/api/products", (req, res) => {
+app.get("/api/products", (res) => {
   res.json("productRoute");
 });
 app.use("/api/carts", cartRoute);
