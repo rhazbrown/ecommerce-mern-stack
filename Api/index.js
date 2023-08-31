e121b45fbb86ffb616ba2a59efd757e15999eed6
rhazbrown <rhazbrown@gmail.com>
2023-08-31 15:40:20 +0100

    Revert "test"
    This reverts commit 0be3a78bb517da38771adb15d60532765b8cde99.

--- Api/index.js
+++ Api/index.js
@@ -18,13 +18,25 @@ mongoose
     console.log(err);
   });
 
-app.use(cors());
-app.use("/", "helooooo");
-app.use("/api/products", productRoute);
+app.use(
+  cors({
+    origin: ["https://ecommerce-mern-stack-api.vercel.app"],
+    methods: ["POST", "GET"],
+    credentials: true,
+  })
+);
+app.get("/api/products", (req, res) => {
+  res.json("productRoutes");
+});
+app.get("/", (req, res) => {
+  res.json("Helo");
+});
 app.use(express.json());
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
-
+app.get("/api/products", (req, res) => {
+  res.json("productRoutes");
+});
 app.use("/api/carts", cartRoute);
 app.use("/api/orders", orderRoute);
 app.use("/api/checkout", stripeRoute);
